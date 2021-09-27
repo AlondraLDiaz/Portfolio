ddbbc5551cd794a67e1e795277daf62ac5534f17
AlondraLDiaz <79497412+AlondraLDiaz@users.noreply.github.com>
2021-09-27 15:04:24 -0600

    mobile menu
    readjusting media quer nav mobile

--- portfolio.js
+++ portfolio.js
@@ -1,1 +1,10 @@
-console.log("hello")
+  // Open and close Menu//
+  function w3_open() {
+    document.getElementById("mobileMenu").style.width = "100%";
+    document.getElementById("mobileMenu").style.display = "block";
+  }
+  
+  function w3_close() {
+    document.getElementById("mobileMenu").style.display = "none";
+  }
+  
